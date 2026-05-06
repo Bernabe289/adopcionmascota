63a84f6457f4a744b68c8e8f94a9ec6cb27d58d3
Karen Aranda <karenaranda@MacBook-Air-de-Karen.local>
2026-05-06 12:09:40 -0400

    Initial commit

---
+++ src/main/java/com/adopcion/adopcionmascota/AdopcionmascotaApplication.java
@@ -0,0 +1,13 @@
+package com.adopcion.adopcionmascota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopcionmascotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopcionmascotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adopcion/adopcionmascota/AdopcionmascotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adopcion.adopcionmascota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopcionmascotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

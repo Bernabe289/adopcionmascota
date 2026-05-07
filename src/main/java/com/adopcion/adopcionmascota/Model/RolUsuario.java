440cc670f13baf5f316cce5134138a9b63a35d76
Bernabe289 <cb.flores@duocuc.cl>
2026-05-06 23:16:09 -0400

    Configura conexión MySQL y crea entidad RolUsuario

---
+++ src/main/java/com/adopcion/adopcionmascota/Model/RolUsuario.java
@@ -0,0 +1,22 @@
+package com.adopcion.adopcionmascota.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "rol_usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RolUsuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_rol")
+    private Integer idRol;
+
+    @Column(name= "nombre_rol", nullable = false, unique = true, length = 50)
+    private String nombreRol;
+
+}

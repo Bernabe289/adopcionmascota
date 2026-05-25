50c10e057a5028d10038cea08604e7d5b26488ed
Bernabe289 <cb.flores@duocuc.cl>
2026-05-24 22:52:55 -0400

    Gabriel Bustamante

--- visitaservice/src/main/java/com/example/visitaservice/Repository/VisitaRepository.java
+++ visitaservice/src/main/java/com/example/visitaservice/Repository/VisitaRepository.java
@@ -1,4 +1,10 @@
 package com.example.visitaservice.Repository;
 
-public interface VisitaRepository {
+import com.example.visitaservice.Model.Visita;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitaRepository extends JpaRepository<Visita, Integer> {
+
 }

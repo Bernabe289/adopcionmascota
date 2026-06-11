475d88df4c5428cd1762d606955f573bf7dcfb21
Bernabe289 <cb.flores@duocuc.cl>
2026-06-11 13:42:06 -0400

    Implementa springdoc swagger/open api en rolusuarioservice y especieservice y documenta endpoints en ambos controller

--- especieservice/src/main/java/com/example/especieservice/Controller/EspecieController.java
+++ especieservice/src/main/java/com/example/especieservice/Controller/EspecieController.java
@@ -3,6 +3,10 @@ package com.example.especieservice.Controller;
 
 import com.example.especieservice.Model.Especie;
 import com.example.especieservice.Service.EspecieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,11 +17,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/especies")
+@Tag(name = "Especie", description = "API para la gestion de especies")
 public class EspecieController {
     @Autowired
     private EspecieService especieService;
 
     @GetMapping
+    @Operation(
+            summary = "Obtener especies",
+            description = "Obtiene la lista de todas las especies registradas en el sistema"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No hay especies dentro del sistema"),
+            @ApiResponse(responseCode = "200", description = "Especie encontrada con exito")
+    })
     public ResponseEntity<List<Especie>> getEspecies() {
         List<Especie> especies = especieService.listarEspecies();
 
@@ -30,6 +43,14 @@ public class EspecieController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Crear especie",
+            description = "Registra especie dentro del sistema"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", description = "La especie ya se encuentra dentro del sistema"),
+            @ApiResponse(responseCode = "201", description = "Especie creada con exito")
+    })
     public ResponseEntity<?> createEspecie(@Valid @RequestBody Especie especie){
         Especie nuevaEspecie = especieService.guardarEspecie(especie);
 
@@ -42,25 +63,49 @@ public class EspecieController {
                 .body(nuevaEspecie); //201
     }
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Buscar especie",
+            description = "Obtiene especie por el id asociado"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", description = "Error, no se pudo encontrar la especie"),
+            @ApiResponse(responseCode = "200", description = "Especie encontrada con ecxito")
+    })
     public ResponseEntity<Especie> getId(@PathVariable Integer id) {
         Especie especie = especieService.buscarPorId(id);
 
         if (especie == null) {
-            return ResponseEntity.notFound().build(); //200
+            return ResponseEntity.notFound().build(); //409
 
         }
-        return ResponseEntity.ok(especie);
+        return ResponseEntity.ok(especie); //200
     }
         @PutMapping("/{id}")
+        @Operation(
+                summary = "Actualizar especie",
+                description = "Actualizar especie por su id asociado"
+        )
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "404", description = "Error, especie no encontrada"),
+                @ApiResponse(responseCode = "200", description = "Especie actualizada con exito")
+        })
         public ResponseEntity<Especie> updateEspecie (@PathVariable Integer id, @Valid @RequestBody Especie especie){
             Especie especieActualizada = especieService.actualizarEspecie(id, especie);
             if (especieActualizada == null) {
-                return ResponseEntity.notFound().build();
+                return ResponseEntity.notFound().build(); //404
             }
-            return ResponseEntity.ok(especieActualizada);
+            return ResponseEntity.ok(especieActualizada); //200
         }
 
         @DeleteMapping("/{id}")
+        @Operation(
+                summary = "Eliminar especie",
+                description = "Se elimina especie por su id asociado"
+        )
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "404", description = "Error, no se encontro la especie"),
+                @ApiResponse(responseCode = "200", description = "Especie eliminada con exito")
+        })
         public ResponseEntity<String> deleteEspecie (@PathVariable Integer id){
             boolean eliminado = especieService.eliminarEspecie(id);
 
--- rolservice/src/main/java/com/example/rolservice/Controller/RolUsuarioController.java
+++ rolservice/src/main/java/com/example/rolservice/Controller/RolUsuarioController.java
@@ -2,6 +2,10 @@ package com.example.rolservice.Controller;
 
 import com.example.rolservice.Model.RolUsuario;
 import com.example.rolservice.Service.RolUsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,12 +16,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/roles")
+@Tag(name = "Rol Usuario", description = "API para la gestion de roles")
 public class RolUsuarioController {
 
     @Autowired
     private RolUsuarioService rolUsuarioService;
 
     @GetMapping
+    @Operation(
+            summary = "Listar roles",
+            description = "Obtiene la lista de todos los roles registrados en el sistema."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Roles encontrados con exito"),
+            @ApiResponse(responseCode = "204", description = "No hay roles dentro del sistema")
+    })
     public ResponseEntity<List<RolUsuario>> getRol(){
         List<RolUsuario> roles = rolUsuarioService.listarRoles();
 
@@ -29,6 +42,14 @@ public class RolUsuarioController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Crear rol",
+            description = "Registra roles dentro del sistema"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", description = "Este rol ya se encuentra dentro del sistema"),
+            @ApiResponse(responseCode = "201", description = "Rol creado con exito")
+    })
     public ResponseEntity<?> createRol(@Valid @RequestBody RolUsuario rolUsuario){
         RolUsuario nuevoRol = rolUsuarioService.guardarRol(rolUsuario);
 
@@ -39,6 +60,14 @@ public class RolUsuarioController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Buscar rol",
+            description = "Buscar un rol por su id asociado"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Error, rol no encontrado"),
+            @ApiResponse(responseCode = "200", description = "Rol encontrado dentro del sistema")
+    })
     public ResponseEntity<RolUsuario> getId(@PathVariable Integer id){
         RolUsuario rolUsuario = rolUsuarioService.buscarPorId(id);
 
@@ -50,6 +79,14 @@ public class RolUsuarioController {
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Actualizar rol",
+            description = "Actualiza el rol por su id asociado"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Error, rol no encontrado"),
+            @ApiResponse(responseCode = "200", description = "Rol actualizado con exito")
+    })
     public ResponseEntity<RolUsuario> updateRol(@PathVariable Integer id, @Valid @RequestBody RolUsuario rolUsuario){
         RolUsuario rolActualizado = rolUsuarioService.actualizarRol(id, rolUsuario);
 
@@ -61,6 +98,14 @@ public class RolUsuarioController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Eliminar rol",
+            description = "Elimina el rol asociado por su id"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Error, rol no encontrado"),
+            @ApiResponse(responseCode = "200", description = "Rol eliminado con exito")
+    })
     public ResponseEntity<String> deleteRol(@PathVariable Integer id){
         boolean eliminado = rolUsuarioService.eliminarRol(id);
 

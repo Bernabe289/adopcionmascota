2b277afcb626762962d82dbd9ed43fa956c5961e
Bernabe289 <cb.flores@duocuc.cl>
2026-06-18 17:22:11 -0400

    Implementa testing en Especie service y Refugio service

--- especieservice/src/test/java/com/example/especieservice/EspecieserviceApplicationTests.java
+++ especieservice/src/test/java/com/example/especieservice/EspecieserviceApplicationTests.java
@@ -1,13 +1,80 @@
 package com.example.especieservice;
 
+import com.example.especieservice.Model.Especie;
+import com.example.especieservice.Repository.EspecieRepository;
+import com.example.especieservice.Service.EspecieService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class EspecieserviceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Mock
+    EspecieRepository especieRepository;
+
+    @InjectMocks
+    EspecieService especieService;
+
+    private Especie especie;
+
+    @BeforeEach
+    void setUp(){
+        especie = new Especie();
+        especie.setIdEspecie(1);
+        especie.setNombreEspecie("PERRO");
     }
 
+    @Test
+    @DisplayName("Buscar especie por ID existente")
+    void buscarEspeciePorIdExistente(){
+        //Given
+        when(especieRepository.findById(1)).thenReturn(Optional.of(especie));
+
+        //When
+        Especie resultado = especieService.buscarPorId(1);
+
+        //Then
+        assertNotNull(resultado);
+        assertEquals(1, resultado.getIdEspecie());
+        assertEquals("PERRO", resultado.getNombreEspecie());
+    }
+
+    @Test
+    @DisplayName("Buscar especie por ID inexistente")
+    void buscarEspeciePorIdInexistente(){
+        //Given
+        when(especieRepository.findById(99)).thenReturn(Optional.empty());
+
+        //When
+        Especie resultado = especieService.buscarPorId(99);
+
+        //Then
+        assertNull(resultado);
+    }
+
+    @Test
+    @DisplayName("Guardar especie correctamente")
+    void guardarEspecieCorrectamente() {
+        //Given
+        when(especieRepository.save(especie)).thenReturn(especie);
+
+        //When
+        Especie resultado = especieService.guardarEspecie(especie);
+
+        //Then
+        assertNotNull(resultado);
+        assertEquals(1, resultado.getIdEspecie());
+        assertEquals("PERRO", resultado.getNombreEspecie());
+    }
 }
--- refugioservice/src/test/java/com/example/refugioservice/RefugioserviceApplicationTests.java
+++ refugioservice/src/test/java/com/example/refugioservice/RefugioserviceApplicationTests.java
@@ -1,13 +1,83 @@
 package com.example.refugioservice;
 
+import com.example.refugioservice.Model.Refugio;
+import com.example.refugioservice.Repository.RefugioRepository;
+import com.example.refugioservice.Service.RefugioService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
 class RefugioserviceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Mock
+    RefugioRepository refugioRepository;
+
+    @InjectMocks
+    RefugioService refugioService;
+
+    private Refugio refugio;
+
+    @BeforeEach
+    void setUp(){
+        refugio = new Refugio();
+        refugio.setIdRefugio(1);
+        refugio.setNombreRefugio("Refugio Patitas");
+        refugio.setDireccionRefugio("Calle 1234");
+        refugio.setTelefonoRefugio("987654321");
+        refugio.setEmailRefugio("refugio@gmail.com");
+        refugio.setEstadoRefugio("ACTIVO");
     }
 
+    @Test
+    @DisplayName("Buscar refugio por ID existente")
+    void buscarRefugioPorIdExistente(){
+        //Given
+        when(refugioRepository.findById(1)).thenReturn(Optional.of(refugio));
+
+        Refugio resultado = refugioService.buscarPorId(1);
+
+        assertNotNull(resultado);
+        assertEquals(1, resultado.getIdRefugio());
+        assertEquals("Refugio Patitas", resultado.getNombreRefugio());
+    }
+
+    @Test
+    @DisplayName("Buscar refugio por ID inexistente")
+    void buscarRefugioPorIdInexistente() {
+        //Given
+        when(refugioRepository.findById(99)).thenReturn(Optional.empty());
+
+        //When
+        Refugio resultado = refugioService.buscarPorId(99);
+
+        //Then
+        assertNull(resultado);
+    }
+    @Test
+    @DisplayName("Guardar refugio")
+    void guardarRefugio() {
+        //Given
+        when(refugioRepository.save(refugio)).thenReturn(refugio);
+
+        //When
+        Refugio resultado = refugioService.guardarRefugio(refugio);
+
+        //Then
+        assertNotNull(resultado);
+        assertEquals(1, resultado.getIdRefugio());
+        assertTrue(resultado.getNombreRefugio().equalsIgnoreCase("Refugio Patitas"));
+        assertEquals("refugio@gmail.com", resultado.getEmailRefugio());
+    }
 }
